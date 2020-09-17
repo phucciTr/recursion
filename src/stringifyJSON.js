244eef7a069dd9559760ece902f7f30b62f2ad44
Phuc <miabam26@gmail.com>
2020-09-16 20:04:46 -0700

    Refactored for cleaner code & made test pass

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,55 +6,91 @@
 
 var stringifyJSON = function(obj) {
 
-  // returns (undefined) if a (condition) is (true)
-  if (obj === undefined || typeof obj === 'function' || typeof obj === 'symbol') {
-    return undefined;
+  if (shouldReturnUndefined(obj)) { return undefined; }
+
+  if (Array.isArray(obj)) { return stringifyArray(obj); }
+
+  if (isTypeOfObject(obj)) {
+    let stringifiedObj = checkObjectInstance(obj);
+    if (!isPrimitiveInstance(obj)) { stringifiedObj += '}'; }
+    return stringifiedObj;
   }
 
-  // If (obj) is an []
-  if (Array.isArray(obj)) {
-    return stringifyArray(obj);
-  }
+  if (shouldReturnNull(obj)) { return 'null'; }
 
-  // If (obj) is of (type object), check its (instance)
-  if (typeof obj === 'object') {
-    return checkObjectInstance(obj);
-  }
-
-  // returns (null) if a (condition) is (true)
-  if (obj === null || obj === Infinity) {
-    return null;
-  }
-
-
-  // If (obj) is a (primitive)
-  return stringifyValue(obj);
+  return stringifyPrimitiveVal(obj);
 };
 
+// Helper Func
+var shouldReturnUndefined = function(obj) {
+  return obj === undefined || typeof obj === 'function' ||
+                              typeof obj === 'symbol';
+};
 
-// Stringify (string / number) value
-var stringifyValue = function(obj) {
-  if (typeof obj === 'number' && isNaN(obj)) {
+// Helper Func
+var isTypeOfObject = function(obj) {
+  return typeof obj === 'object' && !shouldReturnNull(obj);
+};
 
-    return null;
-  }
+// Helper Func
+var shouldReturnNull = function(obj) {
+  return obj === null || obj === Infinity ||
+   typeof obj === 'number' && isNaN(obj);
+};
 
-  if (typeof obj === 'string' && obj.indexOf('"') !== -1) {
-    obj = obj.replace(/"/, '"\\');
-    obj = obj.replace(/'""'/, '"\\\"');
-    return obj;
-  }
-  return obj.toString();
+var stringifyArray = function(arrayObj) {
+  let accumulator = '[';
+  accumulator = accumulateArrayElements(arrayObj, accumulator);
+  accumulator += ']';
+  return accumulator;
+};
+
+var accumulateArrayElements = function(array, accumulator) {
+
+  array.forEach(function(current, index) {
+
+    if (Array.isArray(current)) {
+
+      let stringifiedArray = stringifyArray(current);
+      accumulator += stringifiedArray;
+
+    } else if (isTypeOfObject(current)) {
+
+      let stringifiedObj = checkObjectInstance(current);
+      if (!isPrimitiveInstance(current)) { stringifiedObj += '}'; }
+      accumulator += stringifiedObj;
+
+    } else if (shouldReturnNullInsideArray(current)) {
+      accumulator += 'null';
+
+    } else { accumulator += stringifyPrimitiveVal(current); }
+
+    if (!isEndOfArray(index, array)) { accumulator += ','; }
+  });
+
+  return accumulator;
+};
+
+// Helper Func
+var isEndOfArray = function(counter, array) {
+  return counter === array.length - 1;
+};
+
+// Helper Func
+var shouldReturnNullInsideArray = function(value) {
+  return typeof value === 'symbol' || value === null ||
+  value === Infinity || (typeof value === 'number' && isNaN(value)) ||
+  value === undefined || typeof value === 'function';
 };
 
 
 var checkObjectInstance = function(obj) {
 
-  // If (obj) is NOT (purely) an (Object)..
-  // return (obj)'s (value) accordingly
-  if (obj instanceof Boolean || obj instanceof Number ||
-       obj instanceof String || obj instanceof Date ||
-                                obj instanceof RegExp) {
+  if (Array.isArray(obj)) { return stringifyArray(obj); }
+
+  if (isPurelyObj(obj)) { return stringifyObject(obj); }
+
+  if (isPrimitiveInstance(obj)) {
 
     if (obj instanceof Date) {
       return obj.toISOString();
@@ -62,142 +98,143 @@ var checkObjectInstance = function(obj) {
     } else if (obj instanceof RegExp) {
       return '{}';
 
-    } else {
-
-      if (obj instanceof String) {
-
-        return '"' + obj.toString() + '"';
-      }
-
-      return obj.toString();
+    } else if (obj instanceof String) {
+      return '"' + obj.toString() + '"';
     }
-
-  // If (obj) is (purely) an (Object)
-  } else {
-    return stringifyObject(obj);
+    // otherwise
+    return obj.toString();
   }
 };
 
-
-var stringifyObject = function(obj) {
-  let args = arguments;
-
-  let accumulator = '{';
-  accumulator = accumulateObjectElements(obj, accumulator);
-  accumulator += '}';
-  return accumulator;
+// Helper Func
+var isPurelyObj = function(obj) {
+  return !isPrimitiveInstance(obj) && obj !== null && !Array.isArray(obj);
 };
 
-var accumulateObjectElements = function(obj, accumulator) {
-
-  // Iterate thru (obj)
-  for (let key in obj) {
-
-    let currentVal = obj[key];
-
-    if (currentVal === undefined || typeof currentVal === 'function' || typeof currentVal === 'symbol') {
-      continue;
-
-    } else if (typeof currentVal === 'string') {
-      accumulator += '"' + key + '":"' + currentVal + '",';
-
-    } else if (typeof currentVal === 'number') {
-
-      accumulator += '"' + key + '":';
-      accumulator += currentVal === Infinity ? 'null,' : (currentVal + ',');
-
-    } else if (Array.isArray(currentVal)) {
-
-      accumulator += '"' + key + '":';
-      accumulator += stringifyArray(currentVal);
-      accumulator = accumulator.slice(0, accumulator.length - 1);
-      accumulator += ',';
-
-    } else if (typeof currentVal === 'object') {
-
-      accumulator += '"' + key + '":';
-
-      if (!isPrimitiveInstance(currentVal)) {
-        accumulator += '{';
-        accumulator = accumulateObjectElements(currentVal, accumulator);
-        accumulator = accumulator.slice(0, accumulator.length - 1);
-        accumulator += '}';
-
-      } else {
-        accumulator += checkObjectInstance(currentVal);
-      }
-
-    } else if (currentVal === null || currentVal === Infinity || isNaN(currentVal) ) {
-      accumulator += '"' + key + '":"null",';
-    }
-  }
-
-  return accumulator;
-
-};
-
+// Helper Func
 var isPrimitiveInstance = function(obj) {
-
   return obj instanceof Boolean || obj instanceof Number ||
     obj instanceof String || obj instanceof Date ||
                              obj instanceof RegExp;
 };
 
-
-var stringifyArray = function(arrayObj) {
-
-  let accumulator = '[';
-  accumulator = accumulateArrayElements(arrayObj, accumulator);
-
-  accumulator = accumulator.slice(0, accumulator.length - 1);
-  accumulator += ']';
-
+var stringifyObject = function(obj) {
+  let accumulator = '{';
+  accumulator = accumulateObjectElements(obj, accumulator);
   return accumulator;
 };
 
+var accumulateObjectElements = function(obj, accumulator) {
 
-var accumulateArrayElements = function(array, accumulator) {
+  let keys = Object.keys(obj);
 
-  for (let i = 0; i < array.length; i++) {
+  keys.forEach(function(key, index) {
 
-    let current = array[i];
+    let currentVal = obj[key];
 
-    // If (current) is NOT an [] / {}
-    if (!(Array.isArray(current)) || typeof current !== 'object') {
+    if (!shouldReturnNothing(currentVal)) {
 
-      if (typeof current === 'symbol' || current === null ||
-          current === Infinity || isNaN(current) ||
-          current === undefined || typeof current === 'function') {
+      if (isString(currentVal)) {
+        accumulator += '"' + key + '":"' + currentVal + '"';
 
-        accumulator += 'null';
-        accumulator += isEndOfArray(i, array) ? '],' : ',';
+      } else if (isNumber(currentVal)) {
+        accumulator += '"' + key + '":';
+        accumulator += (currentVal === Infinity) ? 'null' : currentVal;
 
-      // (Current) is a (primitive)
-      } else {
-        accumulator += current.toString();
-        accumulator += isEndOfArray(i, array) ? '],' : ',';
+      } else if (isBoolean(currentVal)) {
+        accumulator += '"' + key + '":' + currentVal;
+
+      } else if (Array.isArray(currentVal)) {
+        accumulator += '"' + key + '":';
+        accumulator = accumulateArrayValue(currentVal, index, accumulator);
+
+      } else if (isStringifyableObj(currentVal)) {
+        accumulator += '"' + key + '":';
+        accumulator = accumulateObjValue(currentVal, accumulator);
+
+      } else if (shouldReturnNull(currentVal)) {
+        accumulator += '"' + key + '":null';
       }
 
-    // If (current) is an [], recursively (collect) [] s' (elements) into (accumulator)
-    } else if (Array.isArray(current)) {
-      accumulator += '[';
-      accumulator = accumulateArrayElements(current, accumulator);
-
-    // If (current) is a (type of object), check (array)'s (instance)
-    } else if (typeof current === 'object') {
-      return checkObjectInstance(current);
+      if (!isEndOfArray(index, keys)) { accumulator += ','; }
     }
+  });
+  return accumulator;
+};
 
+// Helper Func
+var shouldReturnNothing = function(value) {
+  return value === undefined || typeof value === 'function' ||
+                                     typeof value === 'symbol';
+};
+
+// Helper Func
+var isString = function(value) {
+  return typeof value === 'string';
+};
+
+// Helper Func
+var isNumber = function(value) {
+  return typeof value === 'number';
+};
+
+// Helper Func
+var isBoolean = function(value) {
+  return typeof value === 'boolean';
+};
+
+// Helper Func
+var isStringifyableObj = function(obj) {
+  return typeof obj === 'object' && obj !== undefined && obj !== null;
+};
+
+// Helper Func
+var accumulateArrayValue = function(array, currentIndex, accumulator) {
+  accumulator += stringifyArray(array);
+  return accumulator;
+};
+
+// Helper Func
+var accumulateObjValue = function(obj, accumulator) {
+
+  if (isPrimitiveInstance(obj)) { accumulator += checkObjectInstance(obj); }
+
+  if (isPurelyObj(obj)) {
+    accumulator += '{';
+    accumulator = accumulateObjectElements(obj, accumulator);
+    accumulator += '}';
   }
   return accumulator;
 };
 
+// Helper Func
+var stringifyPrimitiveVal = function(obj) {
 
+  if (shouldReturnNull(obj)) { return null; }
 
-var isEndOfArray = function(counter, array) {
-  return counter === array.length - 1;
+  if (isString(obj) && !hasSingleQuote(obj) && !hasDoubleQuote(obj)) { return '"' + obj + '"'; }
+
+  if (hasSingleQuote(obj)) { return obj = '"' + obj + '"'; }
+
+  if (hasDoubleQuote(obj)) {
+    obj = obj.replace(/"/g, '\\\"');
+    obj = '"' + obj + '"';
+    return obj;
+  }
+
+  // otherwise
+  return obj.toString();
 };
 
+// Helper Func
+var hasDoubleQuote = function(obj) {
+  return typeof obj === 'string' && obj.indexOf('"') !== -1;
+};
+
+// Helper Func
+var hasSingleQuote = function(obj) {
+  return typeof obj === 'string' && obj.indexOf('\'') !== -1;
+};
 
 
 // Assertion Func()
@@ -210,11 +247,8 @@ var assertEqual = function(actual, expected) {
 };
 
 
-
-
 // Assertions
 var arr = [1, [Infinity, Symbol('')], 3, [null, 1, Infinity, undefined, function() {}], [2, 4, 5]];
-
 var actual = stringifyJSON(obj);
 var expected = JSON.stringify(obj);
 assertEqual(actual, expected);
@@ -223,28 +257,88 @@ assertEqual(actual, expected);
 var obj = {
   string: 'hi',
   number: 3,
-  array: [1, Infinity, 3, 4],
+  array: [1, Infinity, 3, 4, {Infinity: Infinity, string: 'hi'}],
   Infinity: Infinity,
   Undefined: undefined,
   object: {string: 'bye', number: 10, func: function() {}, array: [Infinity, null, undefined]}
 };
-
 var actual = stringifyJSON(obj);
 var expected = JSON.stringify(obj);
 assertEqual(actual, expected);
 
 
 var obj = new String('hello');
-
 var actual = stringifyJSON(obj);
 var expected = JSON.stringify(obj);
 assertEqual(actual, expected);
-
 
 
 var obj = '"hello"';
-
 var actual = stringifyJSON(obj);
 var expected = JSON.stringify(obj);
 assertEqual(actual, expected);
 
+
+var obj = [undefined, {null: null, infinity: Infinity, obj: {array: [{obj: {null: null, array: []}}]}}];
+var actual = stringifyJSON(obj);
+var expected = JSON.stringify(obj);
+assertEqual(actual, expected);
+
+var obj = null;
+var actual = stringifyJSON(obj);
+var expected = JSON.stringify(obj);
+assertEqual(actual, expected);
+
+var obj = '\'Hello world\'';
+var actual = stringifyJSON(obj);
+var expected = JSON.stringify(obj);
+assertEqual(actual, expected);
+
+var obj = 'Hello world';
+var actual = stringifyJSON(obj);
+var expected = JSON.stringify(obj);
+assertEqual(actual, expected);
+
+
+var obj = [1, 2, 3, [8], 9];
+var actual = stringifyJSON(obj);
+var expected = JSON.stringify(obj);
+assertEqual(actual, expected);
+
+
+var obj = [4, '["hi"]', 6, 9, 12];
+var actual = stringifyJSON(obj);
+var expected = JSON.stringify(obj);
+assertEqual(actual, expected);
+
+
+var obj = {
+  'foo': true,
+  'bar': false,
+  'baz': null
+};
+var actual = stringifyJSON(obj);
+var expected = JSON.stringify(obj);
+assertEqual(actual, expected);
+
+
+var obj = {
+  'a': {'b': 'c'}
+};
+var actual = stringifyJSON(obj);
+var expected = JSON.stringify(obj);
+assertEqual(actual, expected);
+
+
+var obj = [
+  {a: 'b'}, {b : 'd'}
+];
+var actual = stringifyJSON(obj);
+var expected = JSON.stringify(obj);
+assertEqual(actual, expected);
+
+
+var obj = {a: [], c: {}, b: true};
+var actual = stringifyJSON(obj);
+var expected = JSON.stringify(obj);
+assertEqual(actual, expected);
